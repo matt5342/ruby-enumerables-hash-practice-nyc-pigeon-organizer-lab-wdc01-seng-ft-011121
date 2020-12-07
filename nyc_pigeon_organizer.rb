ae37740e91f0e6f933e176b2ebab7c2db318715a
Matthew Sewell <mattfsewell@gmail.com>
2020-12-07 16:09:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  
+  data.each_with_object ({}) do |(category, charac_hash), pigeons|
+    
+    charac_hash.each do |charac, name_array|
+
+      name_array.each do |name|
+        if !pigeons[name]
+          pigeons[name] = {}
+        end
+        if !pigeons[name][category]
+          pigeons[name][category] = []
+        end
+      pigeons[name][category] << charac.to_s
+
+      end
+    end
+    pigeons
+  end
+
+  
+  # pigeons = {}
+  # names = []
+  
+  # data.each do |key|
+  #   key.each do |value|
+  #     value.each do |name|
+  #     puts value[name]
+  #     names.include? name 
+  #     names << name
+  #   end
+  #   end
+    
+  # end
+  #puts pigeons
+  #puts names
 end
+
