eb722cf62ee592c5a69506010e5b09a7f2b00395
Matthew Sewell <mattfsewell@gmail.com>
2020-12-07 15:18:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+    pigeon_data = {
+      :color => {
+        :purple => ["Theo", "Peter Jr.", "Lucky"],
+        :grey => ["Theo", "Peter Jr.", "Ms. K"],
+        :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+        :brown => ["Queenie", "Alex"]
+      },
+      :gender => {
+        :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+        :female => ["Queenie", "Ms. K"]
+      },
+      :lives => {
+        "Subway" => ["Theo", "Queenie"],
+        "Central Park" => ["Alex", "Ms. K", "Lucky"],
+        "Library" => ["Peter Jr."],
+        "City Hall" => ["Andrew"]
+      }
+    }
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  pigeons = {}
+  names = []
+  
+  data.each do |key|
+    key.each do |value|
+      value.each do |name|
+      puts value[name]
+      names.include? name 
+      names << name
+    end
+    end
+    
+  end
+  #puts pigeons
+  #puts names
 end
+
+nyc_pigeon_organizer(pigeon_data)
