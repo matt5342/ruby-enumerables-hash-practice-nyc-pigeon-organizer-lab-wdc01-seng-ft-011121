25df3f018f4fdf3ba34c1bcccf6cac8bc1badf44
Matthew Sewell <mattfsewell@gmail.com>
2020-12-07 15:50:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -25,8 +25,9 @@ def nyc_pigeon_organizer(data)
   data.each_with_object ({}) do |(category, charac), pigeons|
     
     charac.each do |name_array|
+      puts name_array
       name_array.each do |name|
-        puts name
+        # puts name
       end
     end
     
